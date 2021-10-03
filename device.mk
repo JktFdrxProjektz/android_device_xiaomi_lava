ec1c6b4e71dfeac5e0dbe91f98414f69ae80709b
FaaRlxSqd <rafaibrahim1211@gmail.com>
2021-10-04 01:47:54 +0700

    penting commit
    Signed-off-by: FaaRlxSqd <rafaibrahim1211@gmail.com>

--- device.mk
+++ device.mk
@@ -58,6 +58,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/rootdir/etc/fstab.mt6768:$(TARGET_COPY_OUT_RAMDISK)/fstab.mt6768
 
+# Apn
+PRODUCT_COPY_FILES += \
+   $(DEVICE_PATH)/configs/apns-conf.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/apns-conf.xml
+
 # Keylayout
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/uinput-fpc.kl \
